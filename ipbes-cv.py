73b7a22906a4c6c94a89c263d14e839b855f2f51
Rich Sharp <richpsharp@gmail.com>
2017-06-07 00:01:26 -0700

    adding only those grids that intersect the land polygons

--- ipbes-cv.py
+++ ipbes-cv.py
@@ -5,6 +5,8 @@ import math
 from osgeo import osr
 from osgeo import ogr
 import rtree
+import shapely
+import shapely.wkt
 
 _TARGET_WORKSPACE = "ipbes_cv_workspace"
 
@@ -24,32 +26,36 @@ _WGS84_GRID_SIZE = 0.5
 _UTM_GRID_SIZE = 250
 
 _GLOBAL_GRID_VECTOR_FILE_PATTERN = 'global_grid.shp'
-
+_GLOBAL_FEATURE_INDEX_FILE_PATTERN = 'global_feature_index'
 
 def main():
     """Entry point."""
+    if not os.path.exists(_TARGET_WORKSPACE):
+        os.makedirs(_TARGET_WORKSPACE)
+
     global_vector = ogr.Open(_GLOBAL_POLYGON_PATH)
     global_layer = global_vector.GetLayer()
 
-    print 'building global index'
-    global_feature_index = rtree.index.Index()
-    for global_feature in global_layer:
-        feature_geometry = global_feature.GetGeometryRef()
-        # minx maxx miny maxy
-        feature_envelope = feature_geometry.GetEnvelope()
-        # left, bottom, right, top
-        global_feature_index.insert(
-            global_feature.GetFID(), (
-                feature_envelope[0], feature_envelope[2],
-                feature_envelope[1], feature_envelope[3]))
-    print global_layer.GetFeatureCount()
+    global_feature_index_path = os.path.join(
+        _TARGET_WORKSPACE, _GLOBAL_FEATURE_INDEX_FILE_PATTERN)
+    index_pre_built = os.path.exists(global_feature_index_path + '.dat')
+    global_feature_index = rtree.index.Index(global_feature_index_path)
 
-    sys.exit()
+    if not index_pre_built:
+        print 'building global index'
+        for global_feature in global_layer:
+            feature_geometry = global_feature.GetGeometryRef()
+            # minx maxx miny maxy
+            feature_envelope = feature_geometry.GetEnvelope()
+            # left, bottom, right, top
+            global_feature_index.insert(
+                global_feature.GetFID(), (
+                    feature_envelope[0], feature_envelope[2],
+                    feature_envelope[1], feature_envelope[3]))
+    else:
+        print 'global index pre-built'
 
-
-
-    if not os.path.exists(_TARGET_WORKSPACE):
-        os.makedirs(_TARGET_WORKSPACE)
+    print global_feature_index.bounds
 
     n_rows = int((
         _GLOBAL_BOUNDING_BOX_WGS84[3]-_GLOBAL_BOUNDING_BOX_WGS84[1]) / float(
@@ -82,15 +88,22 @@ def main():
 
     global_grid_defn = global_grid_layer.GetLayerDefn()
 
+    prepared_geometry = {}
     for cell_index in xrange(n_rows * n_cols):
         row_index = cell_index / n_cols
         col_index = cell_index % n_cols
+        # xmin, ymin xmax, ymax
         local_bounding_box_wgs84 = [
             _GLOBAL_BOUNDING_BOX_WGS84[0]+col_index*_WGS84_GRID_SIZE,
             _GLOBAL_BOUNDING_BOX_WGS84[1]+row_index*_WGS84_GRID_SIZE,
             _GLOBAL_BOUNDING_BOX_WGS84[0]+(col_index+1)*_WGS84_GRID_SIZE,
             _GLOBAL_BOUNDING_BOX_WGS84[1]+(row_index+1)*_WGS84_GRID_SIZE]
 
+        intersections = list(
+            global_feature_index.intersection(local_bounding_box_wgs84))
+
+        if len(intersections) == 0:
+            continue
         ring = ogr.Geometry(ogr.wkbLinearRing)
         ring.AddPoint(
             local_bounding_box_wgs84[0], local_bounding_box_wgs84[1])
@@ -102,15 +115,26 @@ def main():
             local_bounding_box_wgs84[0], local_bounding_box_wgs84[3])
         ring.AddPoint(
             local_bounding_box_wgs84[0], local_bounding_box_wgs84[1])
-        poly = ogr.Geometry(ogr.wkbPolygon)
-        poly.AddGeometry(ring)
+        cell_geometry = ogr.Geometry(ogr.wkbPolygon)
+        cell_geometry.AddGeometry(ring)
+        cell_geometry_shapely = shapely.wkt.loads(
+            cell_geometry.ExportToWkt())
 
-        # add new geom to layer
-        grid_feature = ogr.Feature(global_grid_defn)
-        grid_feature.SetGeometry(poly)
-        global_grid_layer.CreateFeature(grid_feature)
-        grid_feature = None
-
+        print 'testing %s' % intersections
+        for fid in intersections:
+            global_feature = global_layer.GetFeature(fid)
+            feature_geometry = global_feature.GetGeometryRef()
+            if fid not in prepared_geometry:
+                prepared_geometry[fid] = shapely.wkt.loads(
+                    feature_geometry.ExportToWkt())
+            prepared_shapely_feature = prepared_geometry[fid]
+            if prepared_shapely_feature.intersects(cell_geometry_shapely):
+                # add new geom to layer
+                grid_feature = ogr.Feature(global_grid_defn)
+                grid_feature.SetGeometry(cell_geometry)
+                global_grid_layer.CreateFeature(grid_feature)
+                grid_feature = None
+                break
 
 def _get_utm_spatial_reference(bounding_box):
     """Determine UTM spatial reference given lat/lng bounding box.
